57ba80466b593cb8cd754abb60bc2ebf063fd1af
Rajesh Radhakrishnan <enthoughts@gmail.com>
2024-10-03 15:23:36 -0700

    added data connector profile

--- site-modules/profile/manifests/data_connector.pp
+++ site-modules/profile/manifests/data_connector.pp
@@ -1,5 +1,8 @@
-  class { "puppet_data_connector":
-    dropzone => "/opt/puppetlabs/puppet/puppet_data_connector",
-    metric_filter => ["events","time"], # optional
-    events_filter => ["success"], # optional
+  
+  class profile::data_connector {
+    class { "puppet_data_connector":
+      dropzone => "/opt/puppetlabs/puppet/puppet_data_connector",
+      metric_filter => ["events","time"], # optional
+      events_filter => ["success"], # optional
+    }
   }
