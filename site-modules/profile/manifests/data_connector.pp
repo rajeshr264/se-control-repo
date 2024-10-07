6d935e99782ed26dba5760aef3a57b6c318b5f81
Rajesh Radhakrishnan <enthoughts@gmail.com>
2024-10-07 16:53:26 -0700

    removed filters in data connector

--- site-modules/profile/manifests/data_connector.pp
+++ site-modules/profile/manifests/data_connector.pp
@@ -2,7 +2,5 @@
 class profile::data_connector {
   class { 'puppet_data_connector':
     dropzone => '/opt/puppetlabs/puppet/puppet_data_connector',
-    metric_filter => ["events","time"], # optional
-    events_filter => ["success"], # optional
   }
 }
